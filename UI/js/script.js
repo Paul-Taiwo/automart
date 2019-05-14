ee57340376f71b58dae582ba15092bd61a13d566
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-14 13:17:07 +0100

    feature(landing-page): create UI elements
    - add signup and login modal
    
    [Finishes #165952394]

--- UI/js/script.js
+++ UI/js/script.js
@@ -9,11 +9,53 @@ window.onscroll = () => {
 }
 
 document.querySelector("#toggler").addEventListener("click", (e) => {
-  e.preventDefault;
+  e.preventDefault();
   sideMenu.style.right = "0";
 });
 
 document.querySelector("#hideMenu").addEventListener("click", (e) => {
-  e.preventDefault;
+  e.preventDefault();
   sideMenu.style.right = "-240px";
 });
+
+const modalFunction = () => {
+  let loginButton = document.querySelector("#login-button"),
+      signupButton = document.querySelector("#signup-button"),
+      closeModal = document.querySelectorAll(".modal-btn"),
+      noAccount = document.querySelector("#no-account"),
+      haveAccount = document.querySelector("#have-account");
+
+  loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector("#login-modal.modal").style.display = "block";
+  });
+
+  signupButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector("#signup-modal.modal").style.display = "block";
+  });
+
+
+  closeModal[0].addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector("#login-modal.modal").style.display = "none";
+  });
+
+  closeModal[1].addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector("#signup-modal.modal").style.display = "none";
+  });
+
+  noAccount.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector("#signup-modal.modal").style.display = "block";
+    document.querySelector("#login-modal.modal").style.display = "none";
+  });
+
+  haveAccount.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector("#login-modal.modal").style.display = "block";
+    document.querySelector("#signup-modal.modal").style.display = "none";
+  });
+}
+modalFunction();
