9f4dc9268608c77c9bda403d1d137530aeced890
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-25 16:58:08 +0100

    feature(user-sign-up): user can sign up
    - add validation
    
    [Finishes #166248137]

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import validateSignUp from '../middlewares/validateSignUp';
 import userController from '../controllers/users';
 
 const Route = express.Router();
 
-Route.post('/auth/signup', userController.createUser);
+Route.post('/auth/signup', validateSignUp.validate, userController.createUser);
 
 export default Route;
