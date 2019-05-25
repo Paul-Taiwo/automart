38f9153dbb7bce2598c46e7cc58168b7d344e7ad
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-25 16:28:01 +0100

    feature(user-sign-up): user can sign up
    - create model to store data,
    - create .env file
    
    [Finishes #166248137]

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import cors from 'cors';
+import log from 'fancy-log';
+// import dotenv from 'dotenv';
+import indexRoutes from './routes/index';
+import userRoutes from './routes/users';
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+app.use(cors());
+app.use(logger('dev'));
+
+const PORT = process.env.port || 8080;
+
+app.use('/api/v1/', [indexRoutes, userRoutes]);
+app.all('*', (req, res) => res.status(404).json({
+  status: 404,
+  error: 'Bad request',
+}));
+
+app.all('*', (req, res) => res.status(404).json({
+  status: 404,
+  error: 'Bad request',
+}));
+
+app.listen(PORT, () => log.info(`Listening at ${PORT}`));
+
+export default app;
