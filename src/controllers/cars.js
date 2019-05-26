f21c1a6af084d64c87cdde7f1b319eb0e3d37b76
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-27 00:00:25 +0100

    feature(post-car-ads): user can post a car sale AD
    - trim request
    
    [Finishes #166253693]

--- src/controllers/cars.js
+++ src/controllers/cars.js
@@ -4,9 +4,16 @@ const { Cars } = models;
 
 class CarAds {
   static createAd(req, res) {
-    const {
+    let {
       email, manufacturer, model, price, state, year, bodyType,
     } = req.body;
+
+    manufacturer = manufacturer.trim();
+    model = model.trim();
+    price = price.trim();
+    state = state.trim();
+    year = year.trim();
+    bodyType = bodyType.trim();
 
     const adsData = Cars.createCarAds({
       email,
