c8637571a09ccf2357457fe6e47b4e6baba0901c
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-26 23:45:40 +0100

    feature(post-car-ads): user can post a car sale AD
    - create car AD model
    
    [Start #166253693]

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,15 @@
 import { createUser, findEmail, allUsers } from './users';
+import { createCarAds, allCarsAds } from './cars';
 
 const models = {
   User: {
     createUser,
     findEmail,
     allUsers,
+  },
+  Cars: {
+    createCarAds,
+    allCarsAds,
   },
 };
 
