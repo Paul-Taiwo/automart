ea999f56d3026d40d88fb433614cfbd30ccecb45
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-26 01:21:15 +0100

    chore(test-user-sign-up): write test for user sign up endpoint
    - remove a test case
    
    [Finishes #166248119]

--- src/tests/users.js
+++ src/tests/users.js
@@ -6,8 +6,7 @@ import app from '../app';
 chai.use(chaiHttp);
 
 describe('Test Sign up endpoint', () => {
-  let newU;
-  before((done) => {
+  it('Should return an error message if firstname is empty', (done) => {
     chai
       .request(app)
       .post('/api/v1/auth/signup')
@@ -15,27 +14,7 @@ describe('Test Sign up endpoint', () => {
         'Content-type': 'application/json',
       })
       .send({
-        firstname: 'Testertester',
-        lastname: 'Obodokuna',
-        password: 'testTest12345',
-        address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
-        email: 'alagba@gmail.com',
-      })
-      .end((err, res) => {
-        newU = res.body.data;
-        expect(res.statusCode).to.equal(201);
-        done();
-      });
-  });
-  it('Should create an account', (done) => {
-    chai
-      .request(app)
-      .post('/api/v1/auth/signup')
-      .set({
-        'Content-type': 'application/json',
-      })
-      .send({
-        firstname: 'Testertester',
+        firstname: '',
         lastname: 'Obodokuna',
         password: 'testTest12345',
         address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
@@ -43,120 +22,98 @@ describe('Test Sign up endpoint', () => {
       })
       .end((err, res) => {
         expect(res.body).to.be.an('object');
-        expect(res.statusCode).to.equals(201);
-        expect(res.body.data).to.be.an('object');
+        expect(res.body.status).to.equals(400);
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.error).to.equals('Name fields cannot be empty');
         done();
       });
   });
 
-  // it('Should return an error message if firstname is empty', (done) => {
-  //   chai
-  //     .request(app)
-  //     .post('/api/v1/auth/signup')
-  //     .set({
-  //       'Content-type': 'application/json',
-  //     })
-  //     .send({
-  //       firstname: '',
-  //       lastname: 'Obodokuna',
-  //       password: 'testTest12345',
-  //       address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
-  //       email: 'alagba@gmail.com',
-  //     })
-  //     .end((err, res) => {
-  //       expect(res.body).to.be.an('object');
-  //       expect(res.body.status).to.equals(400);
-  //       expect(res.statusCode).to.equal(400);
-  //       expect(res.body.error).to.equals('Name fields cannot be empty');
-  //       done();
-  //     });
-  // });
+  it('Should return an error message if lastname is empty', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'Tester',
+        lastname: '',
+        password: 'testTest12345',
+        address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
+        email: 'alagba@gmail.com',
+      })
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.equals(400);
+        expect(res.body.error).to.equals('Name fields cannot be empty');
+        done();
+      });
+  });
 
-  // it('Should return an error message if lastname is empty', (done) => {
-  //   chai
-  //     .request(app)
-  //     .post('/api/v1/auth/signup')
-  //     .set({
-  //       'Content-type': 'application/json',
-  //     })
-  //     .send({
-  //       firstname: 'Tester',
-  //       lastname: '',
-  //       password: 'testTest12345',
-  //       address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
-  //       email: 'alagba@gmail.com',
-  //     })
-  //     .end((err, res) => {
-  //       expect(res.body).to.be.an('object');
-  //       expect(res.body.status).to.equals(400);
-  //       expect(res.body.error).to.equals('Name fields cannot be empty');
-  //       done();
-  //     });
-  // });
+  it('Should return an error message if password is empty', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'Tester',
+        lastname: 'Obodokuna',
+        password: '',
+        address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
+        email: 'alagba@gmail.com',
+      })
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.equals(400);
+        expect(res.body.error).to.equals('Password field cannot be empty');
+        done();
+      });
+  });
 
-  // it('Should return an error message if password is empty', (done) => {
-  //   chai
-  //     .request(app)
-  //     .post('/api/v1/auth/signup')
-  //     .set({
-  //       'Content-type': 'application/json',
-  //     })
-  //     .send({
-  //       firstname: 'Tester',
-  //       lastname: 'Obodokuna',
-  //       password: '',
-  //       address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
-  //       email: 'alagba@gmail.com',
-  //     })
-  //     .end((err, res) => {
-  //       expect(res.body).to.be.an('object');
-  //       expect(res.body.status).to.equals(400);
-  //       expect(res.body.error).to.equals('Password field cannot be empty');
-  //       done();
-  //     });
-  // });
+  it('Should return an error message if password is less than 8 characters', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'Tester',
+        lastname: 'Obodokuna',
+        password: 'test12',
+        address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
+        email: 'alagba@gmail.com',
+      })
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.equals(400);
+        expect(res.body.error).to.equals('Password cannot be less than 8 characters');
+        done();
+      });
+  });
 
-  // it('Should return an error message if password is less than 8 characters', (done) => {
-  //   chai
-  //     .request(app)
-  //     .post('/api/v1/auth/signup')
-  //     .set({
-  //       'Content-type': 'application/json',
-  //     })
-  //     .send({
-  //       firstname: 'Tester',
-  //       lastname: 'Obodokuna',
-  //       password: 'test12',
-  //       address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
-  //       email: 'alagba@gmail.com',
-  //     })
-  //     .end((err, res) => {
-  //       expect(res.body).to.be.an('object');
-  //       expect(res.body.status).to.equals(400);
-  //       expect(res.body.error).to.equals('Password cannot be less than 8 characters');
-  //       done();
-  //     });
-  // });
-
-  // it('Should return an error message if email is not valid', (done) => {
-  //   chai
-  //     .request(app)
-  //     .post('/api/v1/auth/signup')
-  //     .set({
-  //       'Content-type': 'application/json',
-  //     })
-  //     .send({
-  //       firstname: 'Tester',
-  //       lastname: 'Obodokuna',
-  //       password: 'testTest12345',
-  //       address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
-  //       email: 'alagbagmail.com',
-  //     })
-  //     .end((err, res) => {
-  //       expect(res.body).to.be.an('object');
-  //       expect(res.body.status).to.equals(400);
-  //       expect(res.body.error).to.equals('Please provide a valid email address');
-  //       done();
-  //     });
-  // });
+  it('Should return an error message if email is not valid', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'Tester',
+        lastname: 'Obodokuna',
+        password: 'testTest12345',
+        address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
+        email: 'alagbagmail.com',
+      })
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.equals(400);
+        expect(res.body.error).to.equals('Please provide a valid email address');
+        done();
+      });
+  });
 });
