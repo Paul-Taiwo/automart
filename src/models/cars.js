05885d1b40439792afa2bb80984f459628797956
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-26 22:58:29 +0100

    feature(post-car-ads): user can post a car sale AD
    - create model for car ads
    
    [Start #166253693]

---
+++ src/models/cars.js
@@ -0,0 +1,24 @@
+const allCarsAds = [];
+
+const createCarAds = (data = null) => {
+  if (!data) {
+    throw new Error('Please provide an object');
+  }
+
+  const id = parseInt(allCarsAds.length + 1123420, 10);
+  const carData = {
+    id,
+    ...data,
+    created_on: new Date().toISOString(),
+    status: 'available',
+  };
+
+  allCarsAds.push(carData);
+
+  return carData;
+};
+
+export {
+  createCarAds,
+  allCarsAds,
+};
