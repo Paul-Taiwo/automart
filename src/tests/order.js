27a6c92a1c790232c86942c573401f03427e682c
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-27 03:05:32 +0100

    chore(test-create-order-endpoint): write test for create order
    - write test for create order endpoint
    
    [Finishes #166248119]

---
+++ src/tests/order.js
@@ -0,0 +1,79 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import { describe, it } from 'mocha';
+import app from '../app';
+
+chai.use(chaiHttp);
+
+describe('Test for create odrer endpoint', () => {
+  it('Should create an order', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/order')
+      .set({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxMDAwLCJmaXJzdG5hbWUiOiJQYXVsIiwibGFzdG5hbWUiOiJPYm9kb2t1bmEiLCJlbmNyeXB0ZWRQYXNzd29yZCI6IiQyYSQxMCRyMWN2ZFhDQ0s1bldaa2oycmQ0NlZlRUpTeEd6SmNOcG9CaWp5RXhYTFRGLm1oeC4uZXdIZSIsImFkZHJlc3MiOiIxMywgcWVlcnJma2Yga2ZrbWZrbSBrZm1rZm1ra21mbWtmIiwiZW1haWwiOiJwYXVsQGdtYWlsLmNvbSIsImlzQWRtaW4iOmZhbHNlfSwiaWF0IjoxNTU4OTEyODA4LCJleHAiOjE1NTg5MjM2MDh9.ZS813EEUegCYU3suHV1NwunqEZ4RvRzaKyoJ96iwl6E',
+      })
+      .send({
+        carId: '1200034',
+        price: '1450000',
+        priceOffered: '1380000',
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(201);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.equal(201);
+        expect(res.body.data).to.be.an('object');
+        expect(res.body.data.id).to.be.a('number');
+        expect(res.body.data.car_id).to.be.a('number');
+        expect(res.body.data.created_on).to.be.a('string');
+        expect(res.body.data.status).to.be.a('string');
+        expect(res.body.data.price).to.be.a('number');
+        expect(res.body.data.priceOffered).to.be.a('number');
+        done();
+      });
+  });
+
+  it('Should return an error if request is not authorized', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/car')
+      .set({
+        'Content-Type': 'application/json',
+      })
+      .send({
+        carId: '1200034',
+        price: '1450000',
+        priceOffered: '1380000',
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(401);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.equal(401);
+        expect(res.body.error).to.equal('Authentication failed! Please Login again');
+        done();
+      });
+  });
+
+  it('Should return an error if token is not valid', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/car')
+      .set({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ayJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxMDAwLCJmaXJzdG5hbWUiOiJQYXVsIiwibGFzdG5hbWUiOiJPYm9kb2t1bmEiLCJlbmNyeXB0ZWRQYXNzd29yZCI6IiQyYSQxMCRyMWN2ZFhDQ0s1bldaa2oycmQ0NlZlRUpTeEd6SmNOcG9CaWp5RXhYTFRGLm1oeC4uZXdIZSIsImFkZHJlc3MiOiIxMywgcWVlcnJma2Yga2ZrbWZrbSBrZm1rZm1ra21mbWtmIiwiZW1haWwiOiJwYXVsQGdtYWlsLmNvbSIsImlzQWRtaW4iOmZhbHNlfSwiaWF0IjoxNTU4OTEyODA4LCJleHAiOjE1NTg5MjM2MDh9.ZS813EEUegCYU3suHV1NwunqEZ4RvRzaKyoJ96iwl6E',
+      })
+      .send({
+        carId: '1200034',
+        price: '1450000',
+        priceOffered: '1380000',
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(401);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.equal(401);
+        expect(res.body.error).to.equal('Authentication failed! Please Login again');
+        done();
+      });
+  });
+});
