8a3fa361f6afe2a83d75a68b402581dfc73976d6
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-27 19:15:50 +0100

    feature(view-specific-car): user can view a specific car
    - create endpoint
    
    [Finishes #166253734]

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -5,6 +5,7 @@ import checkAuth from '../middlewares/auth';
 const Route = express.Router();
 
 Route.post('/car', checkAuth, carController.createAd);
+Route.get('/car/:id', checkAuth, carController.findSpecificCar);
 Route.patch('/car/:id/status', checkAuth, carController.updateStatus);
 Route.patch('/car/:id/price', checkAuth, carController.updateCarPrice);
 
