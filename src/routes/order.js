5dbf8c8a1ca5ce2c015696ddd551ab63c58de445
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-27 02:49:04 +0100

    feature(create-purchase-order): user can create a purchase order
    - add authorization to protect endpoint
    
    [Finishes #166253694]

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import orderController from '../controllers/order';
-// import checkAuth from '../middlewares/auth';
+import checkAuth from '../middlewares/auth';
 
 const Route = express.Router();
 
-Route.post('/order', orderController.makeOrder);
+Route.post('/order', checkAuth, orderController.makeOrder);
 
 export default Route;
