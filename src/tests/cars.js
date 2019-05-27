709f4fcafc2db8af0609dd28da5c0e5e5f4557d9
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-27 01:37:08 +0100

    chore(test-create-car-ads-endpoint): write test for create car AD  endpoint
    - write test for create car AD  endpoint
    
    [Finishes #166248119]

---
+++ src/tests/cars.js
@@ -0,0 +1,89 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import { describe, it } from 'mocha';
+import app from '../app';
+
+chai.use(chaiHttp);
+
+describe('Test for create car AD endpoint', () => {
+  it('Should create a car AD', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/car')
+      .set({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxMDAwLCJmaXJzdG5hbWUiOiJQYXVsIiwibGFzdG5hbWUiOiJPYm9kb2t1bmEiLCJlbmNyeXB0ZWRQYXNzd29yZCI6IiQyYSQxMCRyMWN2ZFhDQ0s1bldaa2oycmQ0NlZlRUpTeEd6SmNOcG9CaWp5RXhYTFRGLm1oeC4uZXdIZSIsImFkZHJlc3MiOiIxMywgcWVlcnJma2Yga2ZrbWZrbSBrZm1rZm1ra21mbWtmIiwiZW1haWwiOiJwYXVsQGdtYWlsLmNvbSIsImlzQWRtaW4iOmZhbHNlfSwiaWF0IjoxNTU4OTEyODA4LCJleHAiOjE1NTg5MjM2MDh9.ZS813EEUegCYU3suHV1NwunqEZ4RvRzaKyoJ96iwl6E',
+      })
+      .send({
+        manufacturer: 'Toyota',
+        model: 'Corolla',
+        price: '14,500',
+        state: 'new',
+        year: '2018',
+        bodyType: 'Saloon',
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(201);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.equal(201);
+        expect(res.body.data).to.be.an('object');
+        expect(res.body.data.id).to.be.a('number');
+        expect(res.body.data.createdOn).to.be.a('string');
+        expect(res.body.data.manufacturer).to.be.a('string');
+        expect(res.body.data.model).to.be.a('string');
+        expect(res.body.data.price).to.be.a('number');
+        expect(res.body.data.state).to.be.a('string');
+        expect(res.body.data.year).to.be.a('number');
+        done();
+      });
+  });
+
+  it('Should return an error if request is not authorized', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/car')
+      .set({
+        'Content-Type': 'application/json',
+      })
+      .send({
+        manufacturer: 'Toyota',
+        model: 'Corolla',
+        price: '14,500',
+        state: 'new',
+        year: '2018',
+        bodyType: 'Saloon',
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(401);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.equal(401);
+        expect(res.body.error).to.equal('Authentication failed! Please Login again');
+        done();
+      });
+  });
+
+  it('Should return an error if token is not valid', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/car')
+      .set({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ayJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxMDAwLCJmaXJzdG5hbWUiOiJQYXVsIiwibGFzdG5hbWUiOiJPYm9kb2t1bmEiLCJlbmNyeXB0ZWRQYXNzd29yZCI6IiQyYSQxMCRyMWN2ZFhDQ0s1bldaa2oycmQ0NlZlRUpTeEd6SmNOcG9CaWp5RXhYTFRGLm1oeC4uZXdIZSIsImFkZHJlc3MiOiIxMywgcWVlcnJma2Yga2ZrbWZrbSBrZm1rZm1ra21mbWtmIiwiZW1haWwiOiJwYXVsQGdtYWlsLmNvbSIsImlzQWRtaW4iOmZhbHNlfSwiaWF0IjoxNTU4OTEyODA4LCJleHAiOjE1NTg5MjM2MDh9.ZS813EEUegCYU3suHV1NwunqEZ4RvRzaKyoJ96iwl6E',
+      })
+      .send({
+        manufacturer: 'Toyota',
+        model: 'Corolla',
+        price: '14,500',
+        state: 'new',
+        year: '2018',
+        bodyType: 'Saloon',
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(401);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.equal(401);
+        expect(res.body.error).to.equal('Authentication failed! Please Login again');
+        done();
+      });
+  });
+});
