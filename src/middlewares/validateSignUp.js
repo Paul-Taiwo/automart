a26b7db7e069231108a5d40a6705423d8d3b4053
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-28 12:40:48 +0100

    chore(test): rewrite test using TDD style
    [Finishes #166282743]

--- src/middlewares/validateSignUp.js
+++ src/middlewares/validateSignUp.js
@@ -1,49 +1,49 @@
-class Validate {
-  static validate(req, res, next) {
-    const {
-      firstname, lastname, email, password,
-    } = req.body;
 
-    // eslint-disable-next-line no-useless-escape
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const validate = (req, res, next) => {
+  const {
+    firstname, lastname, email, password,
+  } = req.body;
 
-    if (re.test(String(email).toLowerCase()) === false) {
-      return res.status(400).json({
-        status: 400,
-        error: 'Please provide a valid email address',
-      });
-    }
+  // eslint-disable-next-line no-useless-escape
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (!firstname || !lastname) {
-      return res.status(400).json({
-        status: 400,
-        error: 'Name fields cannot be empty',
-      });
-    }
-
-    if (firstname.trim().length <= 2 || lastname.trim().length <= 2) {
-      return res.status(400).json({
-        status: 400,
-        error: 'Name fields cannot be less than 2 characters',
-      });
-    }
-
-    if (!password) {
-      return res.status(400).json({
-        status: 400,
-        error: 'Password field cannot be empty',
-      });
-    }
-
-    if (password.trim().length < 8) {
-      return res.status(400).json({
-        status: 400,
-        error: 'Password cannot be less than 8 characters',
-      });
-    }
-
-    return next();
+  if (re.test(String(email).toLowerCase()) === false) {
+    return res.status(400).json({
+      status: 400,
+      error: 'Please provide a valid email address',
+    });
   }
-}
 
-export default Validate;
+  if (!firstname || !lastname) {
+    return res.status(400).json({
+      status: 400,
+      error: 'Name fields cannot be empty',
+    });
+  }
+
+  if (firstname.trim().length <= 2 || lastname.trim().length <= 2) {
+    return res.status(400).json({
+      status: 400,
+      error: 'Name fields cannot be less than 2 characters',
+    });
+  }
+
+  if (!password) {
+    return res.status(400).json({
+      status: 400,
+      error: 'Password field cannot be empty',
+    });
+  }
+
+  if (password.trim().length < 8) {
+    return res.status(400).json({
+      status: 400,
+      error: 'Password cannot be less than 8 characters',
+    });
+  }
+
+  return next();
+};
+
+
+export default validate;
--- src/tests/index.js
+++ src/tests/index.js
@@ -1,4 +1,4 @@
-import chai, { expect } from 'chai';
+import chai, { expect, assert } from 'chai';
 import chaiHttp from 'chai-http';
 import { describe, it } from 'mocha';
 import app from '../app';
@@ -16,8 +16,12 @@ describe('API Endpoints', () => {
       .end((err, res) => {
         expect(res.body).to.be.an('object');
         expect(res.statusCode).to.equal(404);
-        expect(res.body.error).equal('Bad request');
         expect(res.body.status).to.equal(404);
+        assert.isObject(res.body, 'Response is not an object');
+        assert.strictEqual(res.statusCode, 404, 'Status code is not 404');
+        assert.strictEqual(res.body.status, 404, 'Status is not be 404');
+        assert.strictEqual(res.body.error, 'Bad request', 'Error is not Bad request');
+        assert.isNull(err, 'Expect error to not exist');
         done();
       });
   });
@@ -34,6 +38,11 @@ describe('API Endpoints', () => {
         expect(res.statusCode).to.equal(200);
         expect(res.body.message).to.equal('Welcome to Automart API');
         expect(res.body.status).to.equal('success');
+        assert.isObject(res.body, 'Expect response to be an object');
+        assert.strictEqual(res.statusCode, 200, 'Status code should be 200');
+        assert.strictEqual(res.body.status, 'success', 'Status code should be success');
+        assert.strictEqual(res.body.message, 'Welcome to Automart API', 'Message should be Welcome to Automart API');
+        assert.isNull(err, 'Expect error to not exist');
         done();
       });
   });
