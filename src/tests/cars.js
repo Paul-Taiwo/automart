7a63c747f28374f73880b601ef836298f2e00f23
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-28 03:44:13 +0100

    chore(test): write more test case
    [Finishes #166248119]

--- src/tests/cars.js
+++ src/tests/cars.js
@@ -283,4 +283,20 @@ describe('Test for car AD endpoint', () => {
         done();
       });
   });
+
+  it('Should return all cars between a price range', (done) => {
+    chai
+      .request(app)
+      .get('/api/v1/car?status=available&min_price=10000&max_price=2000000')
+      .set({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxMDAwLCJmaXJzdG5hbWUiOiJQYXVsIiwibGFzdG5hbWUiOiJPYm9kb2t1bmEiLCJlbmNyeXB0ZWRQYXNzd29yZCI6IiQyYSQxMCQyZFFldFgwcGlzV3o4NXN3Zk1QY0UuLzQzQVpYTEF6OTJHM01sb2w5QWZnUDA1RnZZeGNmYSIsImFkZHJlc3MiOiIxMywgcWVlcnJma2Yga2ZrbWZrbSBrZm1rZm1ra21mbWtmIiwiZW1haWwiOiJwYXVsQGdtYWlsLmNvbSIsImlzQWRtaW4iOmZhbHNlfSwiaWF0IjoxNTU4OTMzNjc5LCJleHAiOjE1NTkxMDY0Nzl9.hDYLLjNdBiss5ljRB_rjn5Iz2-AY5aihFDCbYSsUxwE',
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body.data).to.be.an('array');
+        done();
+      });
+  });
 });
