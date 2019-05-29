e46608b110a3d9543ef3913114417ae51560a00a
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-29 19:07:14 +0100

    feature(validation): export all validation functions in an object
    [Start #166279167]

---
+++ src/middlewares/index.js
@@ -0,0 +1,9 @@
+import validateInput from './validateInput';
+import validateName from './validateName';
+import validateEmail from './validateEmail';
+
+export default {
+  Name: validateName,
+  Email: validateEmail,
+  Input: validateInput,
+};
