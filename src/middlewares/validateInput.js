70ca0aab52e0d1c641e12338d04cf38665ffa2df
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-29 18:24:12 +0100

    feature(validation): rename validateSignUp.js to validateInput.js
    [Start #166279167]

--- src/middlewares/validateInput.js
+++ src/middlewares/validateInput.js
@@ -1,18 +1,8 @@
 
 const validate = (req, res, next) => {
   const {
-    firstname, lastname, email, password,
+    firstname, lastname, password,
   } = req.body;
-
-  // eslint-disable-next-line no-useless-escape
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  if (re.test(String(email).toLowerCase()) === false) {
-    return res.status(400).json({
-      status: 400,
-      error: 'Please provide a valid email address',
-    });
-  }
 
   if (!firstname || !lastname) {
     return res.status(400).json({
