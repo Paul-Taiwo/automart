11b93c2a2db4ea781b2b19815516333ef50f594c
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-29 02:47:20 +0100

    feature(flag-ad): add flag method to model
    [Finishes #166278961]

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,7 @@ import {
   createCarAds, updateStatus, updateCarAdPrice, allCarsAds,
 } from './cars';
 import { createOrder, updatePrice, allOrder } from './order';
+import { createFlag, allFlags } from './flags';
 
 const models = {
   User: {
@@ -20,6 +21,10 @@ const models = {
     createOrder,
     updatePrice,
     allOrder,
+  },
+  Flags: {
+    createFlag,
+    allFlags,
   },
 };
 
