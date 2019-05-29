23b75cc5fd3b6e75c261eacee1c7a743eaf70fd8
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-29 20:13:56 +0100

    feature(validation): add input validation to endpoint
    [Finishes #166279167]

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import orderController from '../controllers/order';
 import checkAuth from '../middlewares/auth';
+import validate from '../middlewares/index';
 
 const Route = express.Router();
 
-Route.post('/order', checkAuth, orderController.makeOrder);
-Route.patch('/order/:id/price', checkAuth, orderController.updateOrder);
+Route.post('/order', checkAuth, validate.Input, orderController.makeOrder);
+Route.patch('/order/:id/price', checkAuth, validate.Input, orderController.updateOrder);
 
 export default Route;
