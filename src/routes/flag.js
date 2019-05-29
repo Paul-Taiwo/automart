6d5fa11cf73508ca80bf4901c26e8c0b047930e9
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-29 20:53:49 +0100

    feature(validation): add validation to endpoint
    [Finishes #166279167]

--- src/routes/flag.js
+++ src/routes/flag.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import checkAuth from '../middlewares/auth';
+import validate from '../middlewares/index';
 import Flag from '../controllers/flags';
 
 const Route = express.Router();
 
-Route.post('/flag/report', checkAuth, Flag.createFlag);
+Route.post('/flag/report', checkAuth, validate.Input, Flag.createFlag);
 
 export default Route;
