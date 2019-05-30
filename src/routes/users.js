8232a1679ce7ab2cafca200ac935159eebf57ef5
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-30 14:35:01 +0100

    feature(validation): add validation to routes
    [Finishes #166279167]

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,8 +5,8 @@ import userController from '../controllers/users';
 const Route = express.Router();
 
 
-Route.post('/auth/signup', validate.Name, validate.Email, validate.Input, userController.createUser);
-Route.post('/auth/admin/signup', validate.Name, validate.Email, validate.Input, userController.createUser);
+Route.post('/auth/signup', validate.Name, validate.Email, validate.PassWord, userController.createUser);
+Route.post('/auth/admin/signup', validate.Name, validate.Email, validate.PassWord, userController.createUser);
 Route.post('/auth/signin', validate.Email, userController.login);
 
 export default Route;
