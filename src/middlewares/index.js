0a719c2907d41610de695e903d53c9ef2e408b21
Paul Taiwo <pauladedokun2000@gmail.com>
2019-05-30 13:28:30 +0100

    feature(validation): update
    [Finishes #166279167]

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,13 @@
 import validateInput from './validateInput';
 import validateName from './validateName';
 import validateEmail from './validateEmail';
+import validateStatus from './validateStatus';
+import validatePrice from './ValidatePrice';
 
 export default {
   Name: validateName,
   Email: validateEmail,
   Input: validateInput,
+  Status: validateStatus,
+  Price: validatePrice,
 };
