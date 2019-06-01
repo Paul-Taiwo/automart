ff1af8fb22ab740c4d7ea92d62081e66d225a0e8
Paul Taiwo <pauladedokun2000@gmail.com>
2019-06-01 12:06:55 +0100

    feature(upload-image): use multiparty middleware
    [Finishes #166318629]

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import multipart from 'connect-multiparty';
 import carController from '../controllers/cars';
 import checkAuth from '../middlewares/auth';
 import validate from '../middlewares/index';
 
 const Route = express.Router();
+const multiparty = multipart();
 
-Route.post('/car', checkAuth, validate.Car, carController.createAd);
+Route.post('/car', checkAuth, multiparty, validate.Car, carController.createAd);
 Route.get('/car/:id', checkAuth, carController.findSpecificCar);
 Route.get('/car/', checkAuth, carController.find);
 Route.patch('/car/:id/status', checkAuth, validate.Status, carController.updateStatus);
