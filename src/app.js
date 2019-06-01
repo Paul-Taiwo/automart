cc47cdfa595343926e917031aaa70c26c456c0bc
Paul Taiwo <pauladedokun2000@gmail.com>
2019-06-01 11:40:32 +0100

    feature(upload-image): remove duplicate code
    [Start #166318629]

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import bodyParser from 'body-parser';
 import logger from 'morgan';
 import cors from 'cors';
 import log from 'fancy-log';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import indexRoutes from './routes/index';
 import userRoutes from './routes/users';
 import carRoutes from './routes/cars';
 import orderRoutes from './routes/order';
 import flagRoutes from './routes/flag';
 
+dotenv.config();
 
 const app = express();
 
@@ -25,13 +26,8 @@ const PORT = process.env.port || 8080;
 app.use('/api/v1/', [indexRoutes, userRoutes, carRoutes, orderRoutes, flagRoutes]);
 
 app.all('*', (req, res) => res.status(400).json({
-  status: 400,
-  error: 'Bad request',
-}));
-
-app.all('*', (req, res) => res.status(400).json({
-  status: 400,
-  error: 'Bad request',
+  status: 404,
+  error: 'Not found',
 }));
 
 app.listen(PORT, () => log.info(`Listening at ${PORT}`));
