3afbce8dd9c24a3427235ad7012785543f0013fb
Paul Taiwo <pauladedokun2000@gmail.com>
2019-06-18 15:04:33 +0100

    feature(sign-up): implement JWT authentication
    [Finishes #166731838]

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import { config } from 'dotenv';
 import { error } from 'fancy-log';
@@ -8,9 +9,9 @@ config();
 
 class Users {
   static async createUser(req, res) {
-    let { firstname, lastname, address } = req.body;
-
     const { email, password } = req.body;
+    let { firstname, lastname, address } = req.body;
+    let isAdmin;
 
     // Remove unnecessary spaces
     firstname = firstname.trim().replace(/\s+/g, '');
@@ -20,9 +21,15 @@ class Users {
     // Encrypt password
     const encryptedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
+    if (req.originalUrl === '/api/v1/auth/admin/signup') {
+      isAdmin = true;
+    } else {
+      isAdmin = false;
+    }
+
     const query = `
-      INSERT INTO users(firstname, lastname, email, password, address)
-      VALUES ($1, $2, $3, $4, $5)
+      INSERT INTO users(firstname, lastname, email, password, address, is_admin)
+      VALUES ($1, $2, $3, $4, $5, $6)
       RETURNING id, firstname, lastname, email, address, is_admin ;`;
     const userData = [
       firstname,
@@ -30,26 +37,30 @@ class Users {
       email,
       encryptedPassword,
       address,
+      isAdmin,
     ];
     try {
       const result = await DB.query(query, userData);
+      const user = result.rows[0];
+
+      const token = jwt.sign({ user }, process.env.SECRETKEY, { expiresIn: '48h' });
 
       return res.status(201).json({
         status: 201,
         data: {
-          id: result.rows[0].id,
-          first_name: result.rows[0].firstname,
-          last_name: result.rows[0].lastname,
-          email: result.rows[0].email,
-          address: result.rows[0].address,
-          is_admin: result.rows[0].is_admin,
+          token,
+          id: user.id,
+          first_name: user.firstname,
+          last_name: user.lastname,
+          email: user.email,
+          address: user.address,
+          is_admin: user.is_admin,
         },
       });
     } catch (err) {
-      if (err) {
-        error(err.stack);
-      }
+      error(err.stack);
     }
+
     return res.status(500).json({
       status: 500,
       error: 'Internal Server Error',
