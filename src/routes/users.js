4ee206eda950c8a2b1a61890e967dd4ea8234b5b
Paul Taiwo <pauladedokun2000@gmail.com>
2019-06-18 15:05:23 +0100

    feature(sign-up): create endpoint for admin sign up
    [Finishes #166731838]

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,5 +5,6 @@ import userController from '../controllers/users';
 const Route = express.Router();
 
 Route.post('/auth/signup', validate.Name, validate.Email, validate.PassWord, userController.createUser);
+Route.post('/auth/admin/signup', validate.Name, validate.Email, validate.PassWord, userController.createUser);
 
 export default Route;
