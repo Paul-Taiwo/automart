737ac1ad1597bd636c55cfe082aca33c4e665a90
Paul Taiwo <pauladedokun2000@gmail.com>
2019-06-18 21:14:57 +0100

    test(sign-up): test sign up endpoint
    [Finishes #166731838]

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,23 +1,9 @@
 import validateName from './validateName';
 import validateEmail from './validateEmail';
-import validateStatus from './validateStatus';
-import validatePrice from './validatePrice';
-import validateCar from './validateCar';
-import validateCarId from './validateCarId';
-import validateOrder from './validateOrder';
-import validateFlag from './validateFlag';
-import validateNewPrice from './validateNewPrice';
 import validatePassword from './validatePassword';
 
 export default {
   Name: validateName,
   Email: validateEmail,
   PassWord: validatePassword,
-  Status: validateStatus,
-  Price: validatePrice,
-  Flag: validateFlag,
-  Car: validateCar,
-  CarId: validateCarId,
-  Order: validateOrder,
-  NewPrice: validateNewPrice,
 };
