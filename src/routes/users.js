93faa106d6a20d6d28e1851687eab1467de8956a
Paul Taiwo <pauladedokun2000@gmail.com>
2019-06-19 10:47:18 +0100

    feature(sign-in): user can sign in
    - write test for sign in endpoint
    - implement JWT authentication
    
    [Finishes #166731837]

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,5 +6,6 @@ const Route = express.Router();
 
 Route.post('/auth/signup', validate.Name, validate.Email, validate.PassWord, userController.createUser);
 Route.post('/auth/admin/signup', validate.Name, validate.Email, validate.PassWord, userController.createUser);
+Route.post('/auth/signin', validate.Email, userController.login);
 
 export default Route;
