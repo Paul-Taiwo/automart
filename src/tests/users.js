75f30b5b802d6bf66e51669ef57a6b24570a7e21
Paul Taiwo <pauladedokun2000@gmail.com>
2019-06-19 10:47:18 +0100

    feature(sign-in): user can sign in
    - write test for sign in endpoint
    - implement JWT authentication
    
    [Finishes #166731837]

--- src/tests/users.js
+++ src/tests/users.js
@@ -21,7 +21,7 @@ describe('Test for sign up endpoint', () => {
         lastname: 'Obodokuna',
         password: 'testTest12345',
         address: '13, qeerrfkf kfkmfkm kfmkfmkkmfmkf',
-        email: 'alagba@gmail.com',
+        email: 'tester@gmail.com',
       })
       .end((err, res) => {
         expect(res.statusCode).to.equal(201);
@@ -329,6 +329,95 @@ describe('Test for sign up endpoint', () => {
         assert.strictEqual(res.body.error,
           'Please provide a valid email address',
           'Expect error to be Please provide a valid email address');
+        assert.isNull(err, 'Expect error to not exist');
+        done();
+      });
+  });
+});
+
+describe('Test sign in endpoint', () => {
+  it('should log the user in', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signin')
+      .set({
+        'Content-type': 'application/x-www-form-urlencoded', Accept: 'application/json',
+      })
+      .send({
+        email: 'alagba@gmail.com',
+        password: 'testTest12345',
+      })
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.status).to.equal(200);
+        expect(res.body.data).to.be.an('object');
+        expect(res.body.data.token).to.be.a('string');
+        expect(res.body.data.id).to.be.an('number');
+        expect(res.body.data.first_name).to.be.a('string');
+        expect(res.body.data.first_name).to.be.a('string');
+        assert.strictEqual(res.statusCode, 200, 'Status code is not 200');
+        assert.strictEqual(res.body.status, 200, 'Status is not 200');
+        assert.isObject(res.body, 'Response is not an object');
+        assert.isObject(res.body.data, 'Data is not an object');
+        assert.isString(res.body.data.token, 'Token is not a string');
+        assert.isNumber(res.body.data.id, 'ID is not a number');
+        assert.isString(res.body.data.first_name, 'Firstname is not a string');
+        assert.isString(res.body.data.first_name, 'Last name is not a string');
+        assert.isNull(err, 'Expect error to not exist');
+        done();
+      });
+  });
+
+  it('should return an error if email is not found', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signin')
+      .set({
+        'Content-type': 'application/x-www-form-urlencoded', Accept: 'application/json',
+      })
+      .send({
+        email: 'alagb@gmail.com',
+        password: 'testTest12345',
+      })
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.error).to.equal('Email not found/wrong email address');
+        assert.isObject(res.body, 'Response is not an object');
+        assert.strictEqual(res.statusCode, 400, 'Status code is not 400');
+        assert.strictEqual(res.body.status, 400, 'Status is not 400');
+        assert.strictEqual(res.body.error,
+          'Email not found/wrong email address',
+          'Expect error to be Email not found/wrong email address');
+        assert.isNull(err, 'Expect error to not exist');
+        done();
+      });
+  });
+
+  it('should return an error if password is wrong', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signin')
+      .set({
+        'Content-type': 'application/x-www-form-urlencoded', Accept: 'application/json',
+      })
+      .send({
+        email: 'alagba@gmail.com',
+        password: 'testTest1235',
+      })
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.error).to.equal('Password is incorrect');
+        assert.isObject(res.body, 'Response is not an object');
+        assert.strictEqual(res.statusCode, 400, 'Status code is not 400');
+        assert.strictEqual(res.body.status, 400, 'Status is not 400');
+        assert.strictEqual(res.body.error,
+          'Password is incorrect',
+          'Expect error to be Password is incorrect');
         assert.isNull(err, 'Expect error to not exist');
         done();
       });
