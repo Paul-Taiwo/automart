134729cdba96ad5544a731864796980a73f346a7
Paul Taiwo <pauladedokun2000@gmail.com>
2019-06-21 20:25:48 +0100

    feature(reset-password): users can reset their password
    [Finishes #166318683]

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import validate from '../middlewares/index';
 import userController from '../controllers/users';
+import password from '../services/mailer';
 
 const Route = express.Router();
 
 Route.post('/auth/signup', validate.Name, validate.Email, validate.PassWord, userController.createUser);
 Route.post('/auth/admin/signup', validate.Name, validate.Email, validate.PassWord, userController.createUser);
 Route.post('/auth/signin', validate.Email, userController.login);
+Route.post('/users/:email/reset_password', password.reset);
 
 export default Route;
