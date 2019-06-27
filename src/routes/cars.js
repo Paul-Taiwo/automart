132ce769e6f07f31b067fb6c32a59fefc053d760
Paul Taiwo <pauladedokun2000@gmail.com>
2019-06-27 12:33:40 +0100

    feat(reset-password): update credentials for google oauth
    [Finishes #166318683]

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import multipart from 'connect-multiparty';
+import multer from 'multer';
 import carController from '../controllers/cars';
 import checkAuth from '../middlewares/auth';
 import validate from '../middlewares/index';
 
 const Route = express.Router();
-const multiparty = multipart();
+const storage = multer.memoryStorage();
+const upload = multer({ storage }).array('images');
 
-Route.post('/car', checkAuth, multiparty, validate.Car, carController.createAd);
+Route.post('/car', checkAuth, upload, validate.Car, carController.createAd);
 Route.patch('/car/:id/status', checkAuth, validate.Status, carController.updateStatus);
 Route.patch('/car/:id/price', checkAuth, validate.Price, carController.updateCarPrice);
 Route.get('/car/:id', checkAuth, carController.findSpecificCar);
