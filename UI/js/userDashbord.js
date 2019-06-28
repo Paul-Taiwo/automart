fec32cb719b97a0531c1703be9d7d2ac1627b93f
Paul Taiwo <pauladedokun2000@gmail.com>
2019-06-28 13:41:34 +0100

    display user details on their dashboard

--- UI/js/userDashbord.js
+++ UI/js/userDashbord.js
@@ -5,10 +5,29 @@
  */
 const isDashboard = window.location.href.includes('dashboard.html');
 
+const getUserAds = (userId) => {
+  const token = localStorage.getItem('token');
+  const url = `http://localhost/api/v1/car?owner=${userId}`;
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => response.json())
+    .then((res) => {
+      console.log(res);
+    });
+};
+
+
 if (isDashboard) {
   const firstname = localStorage.getItem('firstname');
-  const lastname = localStorage.getItem('lastname');
-  const address = localStorage.getItem('address');
+  const userId = localStorage.getItem('userId');
 
   document.querySelector('#username').textContent = firstname;
+
+  getUserAds(userId);
 }
